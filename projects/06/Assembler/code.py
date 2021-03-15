15c060a7c6ea386f39dd770b1122ffd80da7630a
jdbuenol <JulianDavidBueno@gmail.com>
2021-03-14 21:12:17 -0500

    Proyecto 6 ahora con diccionarios

--- projects/06/Assembler/code.py
+++ projects/06/Assembler/code.py
@@ -1,100 +1,22 @@
+dest_m = {"" : "000", "M" : "001", "D" : "010", "MD" : "011", "A" : "100", "AM" : "101", "AD" : "110", "AMD" : "111"}
+
 def dest(mnemonic):
-    if mnemonic == "":
-        return "000"
-    if mnemonic[-1] == "\n":
-        mnemonic = mnemonic[:-1]
-    if mnemonic == "M":
-        return "001"
-    if mnemonic == "D":
-        return "010"
-    if mnemonic == "MD":
-        return "011"
-    if mnemonic == "A":
-        return "100"
-    if mnemonic == "AM":
-        return "101"
-    if mnemonic == "AD":
-        return "110"
-    if mnemonic == "AMD":
-        return "111"
+    return dest_m[mnemonic]
+
+jump_m = {"" : "000", "JGT" : "001", "JEQ" : "010", "JGE" : "011", "JLT" : "100", "JNE" : "101", "JLE" : "110", "JMP" : "111"}
 
 def jump(mnemonic):
-    if mnemonic == "":
-        return "000"
-    if mnemonic[-1] == "\n":
-        mnemonic = mnemonic[:-1]
-    if mnemonic == "JGT":
-        return "001"
-    if mnemonic == "JEQ":
-        return "010"
-    if mnemonic == "JGE":
-        return "011"
-    if mnemonic == "JLT":
-        return "100"
-    if mnemonic == "JNE":
-        return "101"
-    if mnemonic == "JLE":
-        return "110"
-    if mnemonic == "JMP":
-        return "111"
+    return jump_m[mnemonic]
+
+comp_m = {"0" : "0101010", "1" : "0111111", "-1" : "0111010", "D" : "0001100", "A" : "0110000",
+"!D" : "0001101", "!A" : "0110001", "-D" : "0001111", "-A" : "0110011", "D+1" : "0011111",
+"A+1" : "0110111", "D-1" : "0001110", "A-1" : "0110010", "D+A" : "0000010", "D-A" : "0010011",
+"A-D" : "0000111", "A&D" : "0000000", "A|D" : "0010101", "M" : "1110000", "!M" : "1110001",
+"-M" : "1110011", "M+1" : "1110111", "M-1" : "1110010", "D+M" : "1000010", "D-M" : "1010011",
+"M-D" : "1000111", "D&M" : "1000000", "D|M" : "1010101"}
 
 def comp(mnemonic):
-    if mnemonic == "0":
-        return "0101010"
-    if mnemonic == "1":
-        return "0111111"
-    if mnemonic == "-1":
-        return "0111010"
-    if mnemonic == "D":
-        return "0001100"
-    if mnemonic == "A":
-        return "0110000"
-    if mnemonic == "!D":
-        return "0001101"
-    if mnemonic == "!A":
-        return "0110001"
-    if mnemonic == "-D":
-        return "0001111"
-    if mnemonic == "-A":
-        return "0110011"
-    if mnemonic == "D+1":
-        return "0011111"
-    if mnemonic == "A+1":
-        return "0110111"
-    if mnemonic == "D-1":
-        return "0001110"
-    if mnemonic == "A-1":
-        return "0110010"
-    if mnemonic == "D+A":
-        return "0000010"
-    if mnemonic == "D-A":
-        return "0010011"
-    if mnemonic == "A-D":
-        return "0000111"
-    if mnemonic == "A&D":
-        return "0000000"
-    if mnemonic == "A|D":
-        return "0010101"
-    if mnemonic == "M":
-        return "1110000"
-    if mnemonic == "!M":
-        return "1110001"
-    if mnemonic == "-M":
-        return "1110011"
-    if mnemonic == "M+1":
-        return "1110111"
-    if mnemonic == "M-1":
-        return "1110010"
-    if mnemonic == "D+M":
-        return "1000010"
-    if mnemonic == "D-M":
-        return "1010011"
-    if mnemonic == "M-D":
-        return "1000111"
-    if mnemonic == "D&M":
-        return "1000000"
-    if mnemonic == "D|M":
-        return "1010101"
+    return comp_m[mnemonic]
 
 def dec_to_bin(number):
     binary = "0"
